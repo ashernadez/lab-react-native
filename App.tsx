2b7a4560bb97dae2787f7fcf8c8a8bc0208f2b8b
ashernadez3 <137932923+ashernadez@users.noreply.github.com>
2024-03-23 18:55:37 -0500

    main
    hola mundo, crear app, componentes, navegacion

---
+++ App.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react'; 
+import {NavigationContainer} from '@react-navigation/native'; 
+import {createNativeStackNavigator} from '@react-navigation/native-stack'; 
+import { HomeScreen } from './src/views/home/home'; 
+import { RegisterScreen } from './src/views/register/Register'; 
+ 
+export type RootStackParamList = { 
+    HomeScreen: undefined; 
+    RegisterScreen: undefined; 
+  }; 
+   
+   
+  const Stack = createNativeStackNavigator <RootStackParamList> 
+  (); 
+   
+  const App = () => { 
+    return ( 
+      <NavigationContainer> 
+        <Stack.Navigator screenOptions={{ 
+          headerShown: false 
+        }}> 
+          <Stack.Screen 
+            name="HomeScreen" 
+            component={HomeScreen} 
+          /> 
+          <Stack.Screen 
+            name="RegisterScreen" 
+            component={RegisterScreen} 
+            options={{ 
+              headerShown: true, 
+              title: 'Registro', 
+            }} 
+          /> 
+   
+        </Stack.Navigator> 
+      </NavigationContainer> 
+    ); 
+  }; 
+   
+  export default App;
